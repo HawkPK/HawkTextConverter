db03874cf145f41c759f6c9ab0dc51ad41ff2af2
Hawk84 <paweljastrzabek@o2.pl>
2018-08-05 10:19:10 +0200

    [#1] Init Project

---
+++ Service/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Service;
+
+namespace Service
+{
+    public class Class1
+    {
+        public bool IsPrime(int candidate)
+        {
+			 if (candidate == 1)
+				{
+					return false;
+				}
+            throw new NotImplementedException("Please create a test first");
+        }
+    }
+}
